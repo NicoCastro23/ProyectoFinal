28f65ee76c1d71ae5b1e4778581c15032674469f
Maryam <maryamtato1509@gmail.com>
2024-11-08 22:45:08 -0500

    cambio securityconfig

--- ProyectoFinalp3/src/main/java/co/edu/uniquindio/ProyectoFinalp3/Config/SecurityConfig.java
+++ ProyectoFinalp3/src/main/java/co/edu/uniquindio/ProyectoFinalp3/Config/SecurityConfig.java
@@ -2,7 +2,6 @@ package co.edu.uniquindio.ProyectoFinalp3.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 // Importaciones de seguridad
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
